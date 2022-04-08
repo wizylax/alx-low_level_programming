7aa93b4b3ede6989aa0a3fd27a43f37c39e6fbad
Miracle Anko <miracleanko@gmail.com>
2022-04-08 08:02:52 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,23 @@ gcc -c $CFILE
 int main(void)
 {
 int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-if (n > 1)
-	printf("%d is positive\n", n);
-else id ( n < 0)
-	printf("%d is negarive\n", n);
-else 
-	printf("%d is zero\n", n);
+		if (n > 1)
+		{
+			printf("%i is positive\n", n);
+		}
+		
+		else if ( n < 0)
+		{
+			printf("%i is negarive\n", n);
+		}
 
-return (0);
+		else
+		{
+			printf("%i is zero\n", n);
+		}
+		
+	return (0);
 }
-
