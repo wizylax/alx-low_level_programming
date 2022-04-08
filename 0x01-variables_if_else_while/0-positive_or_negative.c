49ea11a7ffab1129f46e841fa0201b9007b7aed7
Miracle Anko <miracleanko@gmail.com>
2022-04-08 08:37:48 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 		if (n > 1)
 		{
 			printf("%i is positive\n", n);
-		}	
+		}
 		else if (n < 0)
 		{
 			printf("%i is negative\n", n);
@@ -24,6 +24,5 @@ int main(void)
 			{
 				printf("%i is zero\n", n);
 			}
-		
-	return(0);
+	return (0);
 }
