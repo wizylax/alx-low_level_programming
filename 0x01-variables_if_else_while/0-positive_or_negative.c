68a955221b50a70592ec1de04cfb2c87f5576955
Miracle Anko <miracleanko@gmail.com>
2022-04-08 08:28:36 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,17 @@ int main(void)
 {
 	int n;
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
 		if (n > 1)
 		{
 			printf("%i is positive\n", n);
-		}
-		
+		}	
 		else if ( n < 0)
 		{
 			printf("%i is negative\n", n);
 		}
-
 			else
 			{
 				printf("%i is zero\n", n);
