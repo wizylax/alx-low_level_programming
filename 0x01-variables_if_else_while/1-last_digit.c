49a1bf0bb4dae092c97f989f402ac47f78eb3e2f
Miracle Anko <miracleanko@gmail.com>
2022-04-08 12:56:18 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main Printing the last digit followed by n
+ * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
