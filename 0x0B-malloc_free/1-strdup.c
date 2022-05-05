be2655170a33c0dce788f0a89831e1f614b339f6
Miracle Anko <miracleanko@gmail.com>
2022-05-05 12:33:19 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -31,4 +31,15 @@ char *_strdup(char *str)
 
 	/*check if malloc was successful*/
 	if (nstr == NULL)
+	{
+		return (NULL);
+	}
 
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+
+}
